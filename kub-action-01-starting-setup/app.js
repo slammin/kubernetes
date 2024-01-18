329a8b12c587cbc147bff1c7059081ded5518cac
Lucas Tobey <lucas_tobey2@comcast.com>
2024-01-18 16:49:41 -0700

    init

---
+++ kub-action-01-starting-setup/app.js
@@ -0,0 +1,18 @@
+const express = require('express');
+
+const app = express();
+
+app.get('/', (req, res) => {
+  res.send(`
+    <h1>!!!Hello from this NodeJS app!!!!!!</h1>
+    <p>This is new!!</p>
+    <p>This is even newer!</p>
+    <p>Try sending a request to /error and see what happens</p>
+  `);
+});
+
+app.get('/error', (req, res) => {
+  process.exit(1);
+});
+
+app.listen(8080);
